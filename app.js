0f4cd22ec57206a3f5c140ee0cb18372c6d6b720
Albja <alban.jaunet@sncf.fr>
2023-01-17 12:21:46 +0100

    quest challenge 2

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
+require("dotenv").config();
 const express = require("express");
+const users = require("./users");
+const movieHandlers = require("./movieHandlers");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+    res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
+app.get("/api/users", users.getUsers);
+app.get("/api/users/:id", users.getUsersById);
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
+    if (err) {
+        console.error("Something bad happened");
+    } else {
+        console.log(`Server is listening on ${port}`);
+    }
 });
